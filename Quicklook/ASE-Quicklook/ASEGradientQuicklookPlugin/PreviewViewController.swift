b41529847e50f78f842f30f3c52a1df214ee9cde
Darren Ford <dford_au-reg@yahoo.com>
2022-10-28 11:57:26 +1100

    Add ability to show radial/linear in the quicklook preview

--- Quicklook/ASE-Quicklook/ASEGradientQuicklookPlugin/PreviewViewController.swift
+++ Quicklook/ASE-Quicklook/ASEGradientQuicklookPlugin/PreviewViewController.swift
@@ -28,11 +28,35 @@ import Cocoa
 import Quartz
 
 import ColorPaletteCodable
+import Defaults
+
+extension Defaults.Keys {
+	static let presentation = Key<PresentationType>("presentation", default: .linear)
+	static let flipped = Key<Bool>("flipped", default: false)
+}
+
+enum PresentationType: Int, DefaultsSerializable {
+	case linear = 0
+	case radial = 1
+}
 
 class PreviewViewController: NSViewController, QLPreviewingController {
 	@IBOutlet var gradientView: GradientDisplayView!
+	@IBOutlet weak var gradientStyleSegment: NSSegmentedControl!
 
 	var gradient: PAL.Gradient?
+
+	@objc dynamic var selectedPresentationTag: Int = Defaults[.presentation].rawValue {
+		didSet {
+			self.gradientView.presentationType = PresentationType(rawValue: selectedPresentationTag)!
+		}
+	}
+
+	@objc dynamic var flipGradient: Bool = Defaults[.flipped] {
+		didSet {
+			self.gradientView.flipGradient = flipGradient
+		}
+	}
 
 	override var nibName: NSNib.Name? {
 		return NSNib.Name("PreviewViewController")
@@ -43,28 +67,9 @@ class PreviewViewController: NSViewController, QLPreviewingController {
 		// Do any additional setup after loading the view.
 	}
 
-	/*
-	 * Implement this method and set QLSupportsSearchableItems to YES in the Info.plist of the extension if you support CoreSpotlight.
-	 *
-	 func preparePreviewOfSearchableItem(identifier: String, queryString: String?, completionHandler handler: @escaping (Error?) -> Void) {
-	 // Perform any setup necessary in order to prepare the view.
-
-	 // Call the completion handler so Quick Look knows that the preview is fully loaded.
-	 // Quick Look will display a loading spinner while the completion handler is not called.
-	 handler(nil)
-	 }
-	 */
-
 	func preparePreviewOfFile(at url: URL, completionHandler handler: @escaping (Error?) -> Void) {
-		// Add the supported content types to the QLSupportedContentTypes array in the Info.plist of the extension.
-
-		// Perform any setup necessary in order to prepare the view.
-
-		// Call the completion handler so Quick Look knows that the preview is fully loaded.
-		// Quick Look will display a loading spinner while the completion handler is not called.
-
 		do {
-			try self.configure(for: url)
+			self.gradient = try PAL.Gradient.Decode(from: url)
 			self.gradientView.gradient = self.gradient
 			handler(nil)
 		}
@@ -73,10 +78,6 @@ class PreviewViewController: NSViewController, QLPreviewingController {
 		}
 
 		handler(nil)
-	}
-
-	func configure(for url: URL) throws {
-		self.gradient = try PAL.Gradient.Decode(from: url)
 	}
 }
 
@@ -88,6 +89,20 @@ class GradientDisplayView: NSView {
 	var gradient: PAL.Gradient? {
 		didSet {
 			self._gradient = gradient?.cgGradient()
+		}
+	}
+
+	var presentationType: PresentationType = Defaults[.presentation] {
+		didSet {
+			Defaults[.presentation] = presentationType
+			self.needsDisplay = true
+		}
+	}
+
+	var flipGradient: Bool = Defaults[.flipped] {
+		didSet {
+			Defaults[.flipped] = flipGradient
+			self.needsDisplay = true
 		}
 	}
 
@@ -115,34 +130,97 @@ class GradientDisplayView: NSView {
 		// Give a 1px breathing space around the outside of the drawing
 		let insetRect = self.bounds.insetBy(dx: 1, dy: 1)
 
-		let barHeight = min(200.0, insetRect.height - 4)
+		if presentationType == .linear {
 
-		do {
-			var horizontalRect = insetRect
-			horizontalRect.size.height = barHeight
+			let barHeight = min(200.0, insetRect.height - 4)
 
-			let yOffset = (insetRect.height - barHeight) / 2
+			do {
+				var horizontalRect = insetRect
+				horizontalRect.size.height = barHeight
 
+				let yOffset = (insetRect.height - barHeight) / 2
+
+
+				// The drawing rectangle, WITHOUT the shadow
+				let coreRect = CGRect(
+					x: horizontalRect.origin.x,
+					y: yOffset,
+					width: horizontalRect.width - 3,
+					height: horizontalRect.height - 3
+				)
+
+				// The path for the gradient content
+				let boundsPath = CGPath(roundedRect: coreRect, cornerWidth: 4, cornerHeight: 4, transform: nil)
+
+				// Draw the gradient within the bounds path
+				ctx.savingGState {
+					$0.addPath(boundsPath)
+					$0.clip()
+					$0.drawLinearGradient(
+						g,
+						start: flipGradient ? CGPoint(x: coreRect.maxX, y: coreRect.minY) : CGPoint(x: coreRect.minX, y: coreRect.maxY),
+						end: flipGradient ? CGPoint(x: coreRect.minX, y: coreRect.maxY) : CGPoint(x: coreRect.maxX, y: coreRect.minY),
+						options: [.drawsAfterEndLocation, .drawsBeforeStartLocation]
+					)
+				}
+
+				// Add a shadow to the gradient path.
+				ctx.savingGState {
+					// Add a path around the bounds
+					$0.addPath(CGPath(rect: self.bounds, transform: nil))
+					// Add another path around the bounds path
+					$0.addPath(boundsPath)
+					// Clip the drawing using evenOdd, which means that the clip path is the DIFFERENCE between the two paths
+					$0.clip(using: .evenOdd)
+
+					// Now, draw the shadow
+					$0.addPath(boundsPath)
+					$0.setFillColor(CGColor.white.copy(alpha: 0.3)!)
+					$0.setShadow(offset: CGSize(width: 2, height: -2), blur: 3, color: .black)
+					$0.fillPath()
+				}
+
+				// Draw a border around the gradient path
+				ctx.savingGState {
+					$0.addPath(boundsPath)
+					let alpha: Double = NSWorkspace.shared.accessibilityDisplayShouldIncreaseContrast ? 1.0 : 0.1
+					$0.setStrokeColor(NSColor.textColor.withAlphaComponent(alpha).cgColor)
+					$0.setLineWidth(1)
+					$0.strokePath()
+				}
+			}
+		}
+		else {
+			let radialRect = insetRect.insetBy(dx: 16, dy: 16)
+
+			let w = min(radialRect.width - 3, radialRect.height - 3)
+
+			let midx = (radialRect.width - w) / 2
+			let midy = (radialRect.height - w) / 2
 
 			// The drawing rectangle, WITHOUT the shadow
 			let coreRect = CGRect(
-				x: horizontalRect.origin.x,
-				y: yOffset,
-				width: horizontalRect.width - 3,
-				height: horizontalRect.height - 3
+				x: radialRect.origin.x + midx,
+				y: radialRect.origin.y + 16 + midy,
+				width: w,
+				height: w
 			)
 
+			let maxR = max(coreRect.width, coreRect.height) / 2
+
 			// The path for the gradient content
-			let boundsPath = CGPath(roundedRect: coreRect, cornerWidth: 4, cornerHeight: 4, transform: nil)
+			let boundsPath = CGPath(ellipseIn: coreRect, transform: nil)
 
 			// Draw the gradient within the bounds path
 			ctx.savingGState {
 				$0.addPath(boundsPath)
 				$0.clip()
-				$0.drawLinearGradient(
+				$0.drawRadialGradient(
 					g,
-					start: CGPoint(x: coreRect.minX, y: coreRect.maxY),
-					end: CGPoint(x: coreRect.maxX, y: coreRect.minY),
+					startCenter: CGPoint(x: coreRect.midX, y: coreRect.midY),
+					startRadius: flipGradient ? maxR : 0,
+					endCenter: CGPoint(x: coreRect.midX, y: coreRect.midY),
+					endRadius: flipGradient ? 0 : maxR,
 					options: [.drawsAfterEndLocation, .drawsBeforeStartLocation]
 				)
 			}
